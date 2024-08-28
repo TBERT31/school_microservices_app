e14f8431a367ecf97772e0084da20c831c35f7dc
TBERT31 <berteau.thomas@hotmail.fr>
2024-08-28 09:13:30 +0200

    monorepo aproach

--- discovery/src/main/java/com/tbert31/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/tbert31/discovery/DiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.tbert31.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
