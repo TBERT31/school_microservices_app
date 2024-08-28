238b56656c30c8291b8ad4b052263dac68fed688
TBERT31 <berteau.thomas@hotmail.fr>
2024-08-28 09:18:59 +0200

    Add microservice student with its dependencies

---
+++ student/src/test/java/com/tbert31/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbert31.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
